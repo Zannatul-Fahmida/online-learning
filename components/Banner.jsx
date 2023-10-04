7a4482232495b300c42fef987febf16adee9d2a5
zannatul <jannatulfahmida15@gmail.com>
2023-10-04 17:12:43 +0600

    navbar and banner done

---
+++ components/Banner.jsx
@@ -0,0 +1,16 @@
+import styles from "../styles/Home.module.css";
+
+export default function Banner() {
+  return (
+    <div className={`${styles.bannerBg} text-center md:p-24 p-12 text-white`}>
+      <p className="text-base md:text-xl pb-6">ONLINE TEACHING MARKETPLACE & E-LEARNING PLATFORM</p>
+      <h2 className="text-3xl pb-6">Empower Yourself with Our Dynamic Online Learning Platform</h2>
+      <p className="text-lg pb-6">
+        Experience personalized and interactive education from anywhere in the
+        world with our innovative online learning and teaching platform.
+      </p>
+      <button className={`${styles.darkTealBg} px-6 py-3 rounded md:m-6 m-2`}>Start Teaching</button>
+      <button className={`${styles.tealBg} px-6 py-3 rounded md:m-6 m-2`}>Start Learning</button>
+    </div>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,146 @@
+import Link from "next/link";
+import { BiSearch } from "react-icons/bi";
+import styles from "../styles/Home.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={`${styles.darkTealBg} navbar text-white md:px-12`}>
+      <div className="navbar-start w-full md:w-1/2">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className={`${styles.tealBg} menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52`}
+          >
+            <li>
+              <p>Categories</p>
+              <ul className="p-2">
+                <li>
+                  <Link href="/">Submenu 1</Link>
+                </li>
+                <li>
+                  <Link href="/">Submenu 2</Link>
+                </li>
+                <li>
+                  <Link href="/">Submenu 1</Link>
+                </li>
+                <li>
+                  <Link href="/">Submenu 2</Link>
+                </li>
+                <li>
+                  <Link href="/">Submenu 1</Link>
+                </li>
+                <li>
+                  <Link href="/">Submenu 2</Link>
+                </li>
+                <li>
+                  <Link href="/">Submenu 1</Link>
+                </li>
+                <li>
+                  <Link href="/">Submenu 2</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="btn btn-ghost hover:bg-teal-900 hover:text-white"
+              >
+                Sign In
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className={`btn btn-ghost hover:bg-teal-900 hover:text-white`}
+              >
+                Get Started
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Link className="normal-case text-2xl" href="/">
+          Online Learning
+        </Link>
+        <div className="hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li tabIndex={0}>
+              <details>
+                <summary className="hover:text-white">Categories</summary>
+                <ul
+                  style={{ backgroundColor: "#0f959cbb" }}
+                  className="grid grid-cols-2 gap-4 w-96"
+                >
+                  <li>
+                    <Link href="/">Link 1</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Link 2</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Link 2</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Link 2</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Link 2</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Link 2</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Link 2</Link>
+                  </li>
+                  <li>
+                    <Link href="/">Link 2</Link>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+      <div className="relative">
+          <input
+            type="text"
+            placeholder="Search your courses...."
+            className={`${styles.tealBg} py-1 rounded-full pl-10 w-96`}
+          />
+          <BiSearch
+            className="absolute left-3 top-2 text-gray-500"
+            style={{ pointerEvents: "none" }}
+          />
+        </div>  
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        
+        <Link href="/" className="btn btn-ghost">
+          Sign In
+        </Link>
+        <Link
+          href="/"
+          className={`${styles.tealBg} btn text-white border-0 rounded-md hover:bg-teal-900 ml-3`}
+        >
+          Get Started
+        </Link>
+      </div>
+    </div>
+  );
+}
