1c89c8fb49dafcc0b930b1987bed1ac4d47ede27
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2023-10-05 12:52:43 +0600

    dropdown items,

--- components/Shared/Navbar.jsx
+++ components/Shared/Navbar.jsx
@@ -2,6 +2,49 @@ import Link from "next/link";
 import { BiSearch } from "react-icons/bi";
 import styles from "../../styles/Home.module.css";
 
+const categoriesItems = [
+  {
+    slug: "/",
+    title: "Web Development",
+    icon: "",
+  },
+  {
+    slug: "/",
+    title: "App Development",
+    icon: "",
+  },
+  {
+    slug: "/",
+    title: "Graphic Design",
+    icon: "",
+  },
+  {
+    slug: "/",
+    title: "Product Design",
+    icon: "",
+  },
+  {
+    slug: "/",
+    title: "Wordpress Development",
+    icon: "",
+  },
+  {
+    slug: "/",
+    title: "Cyber Security",
+    icon: "",
+  },
+  {
+    slug: "/",
+    title: "Business and Marketing",
+    icon: "",
+  },
+  {
+    slug: "/",
+    title: "Data Engineering",
+    icon: "",
+  },
+];
+
 export default function Navbar() {
   return (
     <div className={`${styles.darkTealBg} navbar text-white md:px-12`}>
@@ -30,30 +73,14 @@ export default function Navbar() {
             <li>
               <p>Categories</p>
               <ul className="p-2">
-                <li>
-                  <Link href="/">Submenu 1</Link>
-                </li>
-                <li>
-                  <Link href="/">Submenu 2</Link>
-                </li>
-                <li>
-                  <Link href="/">Submenu 1</Link>
-                </li>
-                <li>
-                  <Link href="/">Submenu 2</Link>
-                </li>
-                <li>
-                  <Link href="/">Submenu 1</Link>
-                </li>
-                <li>
-                  <Link href="/">Submenu 2</Link>
-                </li>
-                <li>
-                  <Link href="/">Submenu 1</Link>
-                </li>
-                <li>
-                  <Link href="/">Submenu 2</Link>
-                </li>
+                {categoriesItems.map((item, index) => (
+                  <li
+                    key={index}
+                    className="bg-gray-800/50 rounded-lg my-2 py-2"
+                  >
+                    <Link href={item.slug}>{item.title}</Link>
+                  </li>
+                ))}
               </ul>
             </li>
             <li>
@@ -84,32 +111,13 @@ export default function Navbar() {
                 <summary className="hover:text-white">Categories</summary>
                 <ul
                   style={{ backgroundColor: "#0f959cbb" }}
-                  className="grid grid-cols-2 gap-4 w-96"
+                  className="grid grid-cols-2 gap-3 w-96"
                 >
-                  <li>
-                    <Link href="/">Link 1</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Link 2</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Link 2</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Link 2</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Link 2</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Link 2</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Link 2</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Link 2</Link>
-                  </li>
+                  {categoriesItems.map((item, index) => (
+                    <li key={index} className="bg-gray-800/50 rounded-lg">
+                      <Link href={item.slug}>{item.title}</Link>
+                    </li>
+                  ))}
                 </ul>
               </details>
             </li>
@@ -117,7 +125,7 @@ export default function Navbar() {
         </div>
       </div>
       <div className="navbar-center hidden lg:flex">
-      <div className="relative">
+        <div className="relative">
           <input
             type="text"
             placeholder="Search your courses...."
@@ -127,10 +135,9 @@ export default function Navbar() {
             className="absolute left-3 top-2 text-gray-500"
             style={{ pointerEvents: "none" }}
           />
-        </div>  
+        </div>
       </div>
       <div className="navbar-end hidden lg:flex">
-        
         <Link href="/" className="btn btn-ghost">
           Sign In
         </Link>
