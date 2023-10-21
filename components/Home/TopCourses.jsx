4f6a0ec38699ca522ba96ff91b4d2a9be1463864
zannatul <jannatulfahmida15@gmail.com>
2023-10-21 19:02:44 +0600

    top courses carousel updated

--- components/Home/TopCourses.jsx
+++ components/Home/TopCourses.jsx
@@ -1,4 +1,7 @@
-import Link from "next/link";
+import { Navigation, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
 import styles from "../../styles/Home.module.css";
 import CoursesCard from "./CoursesCard";
 import Image from "next/image";
@@ -32,33 +35,39 @@ const courses = [
 ];
 
 export default function TopCourses() {
-  return (
-    <div
-      className={`${styles.courseBg} flex flex-col justify-center items-center text-center text-white px-12 md:px-24 py-20`}
+  return (  
+    <div className={`${styles.courseBg} text-center text-white px-12 md:px-24 py-20`}>
+    <div className="flex flex-col justify-center items-center">
+    <h2 className="text-3xl">Top Level Courses</h2>
+    <Image className="my-8" width={500} src={img} alt="design" />
+    </div>
+    <Swiper
+      modules={[Navigation, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      breakpoints={{
+        360: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768:{
+          slidesPerView: 3,
+          spaceBetween: 50,
+        }
+      }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      className="my-8"
     >
-      <h2 className="text-3xl">Top Level Courses</h2>
-      <Image className="my-8" width={500} src={img} alt="design" />
-      <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          {courses?.map((course) => (
-            <CoursesCard key={course.title} course={course} />
-          ))}
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <Link
-              href="#slide4"
-              className="btn btn-circle bg-zinc-800 text-white border-0"
-            >
-              ❮
-            </Link>
-            <Link
-              href="#slide2"
-              className="btn btn-circle bg-zinc-800 text-white border-0"
-            >
-              ❯
-            </Link>
-          </div>
-        </div>
-      </div>
+    {
+      courses?.map((course) => (
+        <SwiperSlide key={course.title}>
+        <CoursesCard course={course} />
+        </SwiperSlide>
+      ))
+    }
+    </Swiper>
     </div>
   );
 }
